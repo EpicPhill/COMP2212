a4d96611d08076b60295eb328fd6db23eb625e21
Phill Raynsford <pr6g13@soton.ac.uk>
2015-02-24 08:35:27 +0000

    made append cleaner

--- operations.ml
+++ operations.ml
@@ -1,15 +1,13 @@
 type word = bytes;;
 type lang = word list;;
-let append (l:lang) (c:char) = 
-	let rec appendhelper buffer nl = match buffer with
-		| [] -> nl
-		| h::t -> appendhelper t nl@[h^(String.make 1 c)] in
-	appendhelper l [];;
+let rec append (l:lang) (c:char) = match l with
+	| h::t -> (h^(String.make 1 c)) :: append t c
+	| smaller -> smaller;;
 let order (l:lang) = List.sort compare l;;
 let contains (l:lang) (w:word) = List.exists (fun e -> e = w) l;;
 let rec union (l1:lang) (l2:lang) = match l1 with
 	| [] -> l2
-	| h::t -> if contains l2 h then union t l2 else union t l2@[h];;
+	| h::t -> if contains l2 h then union t l2 else union t (h::l2);;
 let rec intersection (l1:lang) (l2:lang) = match l1 with
 	| h::t -> if contains l2 h then h :: intersection t l2 else intersection t l2
 	| smaller -> smaller;;
