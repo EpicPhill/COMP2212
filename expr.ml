0ad738047d5eb25a5835e619b8f7c53e3c980343
Phill Raynsford <pr6g13@soton.ac.uk>
2015-03-02 22:18:29 +0000

    fixed a couple odd things;

--- expr.ml
+++ expr.ml
@@ -16,7 +16,7 @@ type expr =
     | Input of lang list * int
     | InputLang of expr
     | InputLimit of expr
-    | HeadExpr of lang
+    | HeadExpr of expr
     | GetExpr of expr * expr
     | WordLengthExpr of expr * expr
     | ConsExpr of expr * expr
@@ -210,7 +210,7 @@ let rec eval_helper func_env arg_env term =
 		readin ()
 	| (HeadExpr (l)) ->
 		let (l') = to_lang_or_stuck(l)
-		in Lang (List.hd l') 
+		in Word (List.hd l')
 	| (ConsExpr (l1,l2)) ->
 		let (l1', l2') = to_lang_pair_or_stuck(l1,l2)
 		in Lang (l1'@l2')
