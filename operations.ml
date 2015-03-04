a36f931e6f6afb4e7b488e62064e1621db98c7f0
Phill Raynsford <pr6g13@soton.ac.uk>
2015-03-04 21:55:34 +0000

    fixed reading in of :

--- operations.ml
+++ operations.ml
@@ -28,8 +28,8 @@ let explode s =
 		if curr < 0 then exploded else
 			explodehelper (curr-1) (s.[curr] :: exploded) in
 	explodehelper (String.length s -1) []
-let allowedChars = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z'; ':']
-let makestring (c:char) = if (List.mem c allowedChars) then String.make 1 c else raise (InvalidInput (to_string c))
+let allowedChars = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z']
+let makestring (c:char) = if (c == ':') then "" else if (List.mem c allowedChars) then String.make 1 c else raise (InvalidInput (to_string c))
 let makeword (c:char) (len:int) = String.make len c
 let rec add_char_to_last l c = match l with
 	| [] -> []
